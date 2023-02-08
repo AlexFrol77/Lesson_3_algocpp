52464d9cee5c2c9be3087c6f5f1ef44a6fb01797
Alex Frol <drbo19777@gmail.com>
2023-02-08 16:04:19 +0300

    Change Task_2 quick_sort

--- Task_2/Task_2/Task_2.cpp
+++ Task_2/Task_2/Task_2.cpp
@@ -1,44 +1,54 @@
 ﻿#include <iostream>
-
-void quick_sort(int* arr, int size) {
-	int start = 0,
-		temp = 0,
-	    finish = size - 1,
-		middle = arr[finish / 2];
-	while (arr[start] <= middle) {
-			start++;
-	}
-	while (middle >= arr[finish]) {
-			finish--;
-	}
-	temp = arr[start];
-	arr[start] = arr[finish];
-	arr[finish] = temp;
-	if (start <= finish) {
-		quick_sort(arr, size);
-	}
-}
+#include <Windows.h>
 
 void print(int* arr, int size) {
-	for (int i = 0; i < size - 1; i++) {
-		std::cout << arr[i] << " ";
-	}
-	std::cout << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int partition(int* arr, int left, int right) {
+    int pivot = arr[right - 1];
+    int i = left;
+  
+    for (int j = left; j < right - 1; j++) {
+        if (arr[j] <= pivot) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+        }
+    }
+    int temp = arr[i];
+    arr[i] = arr[right - 1];
+    arr[right - 1] = temp;
+    return i;
+}
+
+void quick_sort(int* arr, int left, int right) {
+    if (left < right) {
+        int pi = partition(arr, left, right);
+        quick_sort(arr, left, pi);
+        quick_sort(arr, pi + 1, right);
+    }
 }
 
 int main(int argc, char** argv) {
-	setlocale(LC_ALL, "Rus");
-	
-	int arr[] = { 3, 43, 38, 29, 18, 72, 57, 61, 2, 33 };
-	int size = sizeof(arr) / sizeof(arr[0]);
 
-	std::cout << "Исходный массив: ";
-	print(arr, size);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	quick_sort(arr, size);
+    int arr[] = { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-	std::cout << "Отсортированный массив: ";
-	print(arr, size);
+    std::cout << "Исходный массив: ";
+    print(arr, size);
 
-	return 0;
+    quick_sort(arr, 0, size);
+
+    std::cout << "Отсортированный массив: ";
+    print(arr, size);
+
+    return 0;
 }
